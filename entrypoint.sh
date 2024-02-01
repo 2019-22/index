e156b8cde6a069e2733ff8667eb0af602ac8df5e
2019-22 <121666723+2019-22@users.noreply.github.com>
2024-02-01 21:20:26 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-curl "https://pastebin.com/raw/bjzadHka" >status
-xargs <status curl
-cat status | grep 'working\|maintenance'
-rclone version
-rclone config create 'CLOUDNAME' 'pcloud' 'user' $UserName 'pass' $PassWord
 rclone version
 rclone serve http CLOUDNAME: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes
