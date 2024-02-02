3e8b5b75f4918d14e5d9e55fc50ad379bd2d0ca8
2019-22 <121666723+2019-22@users.noreply.github.com>
2024-02-02 14:12:28 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rclone version
-rclone serve http 01: --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 1G --vfs-cache-mode writes
+rclone serve http 01: --addr :$PORT --buffer-size 25G --dir-cache-time 12h --vfs-read-chunk-size 25G --vfs-read-chunk-size-limit 100G --vfs-cache-mode writes
